4db178d82129c62d69c3414d35035c6ad56fce8a
Carolynembugua <mbuguacarolyne6@gmail.com>
2024-09-04 16:33:20 +0300

    first commit

---
+++ app/src/main/java/com/example/mindtivity/navigation/AppNavHost.kt
@@ -0,0 +1,56 @@
+package com.example.mindtivity.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.mindtivity.ui.theme.screens.about.AboutScreen
+import com.example.mindtivity.ui.theme.screens.home.HomeScreen
+import com.example.mindtivity.ui.theme.screens.login.LoginScreen
+import com.example.mindtivity.ui.theme.screens.products.AddProductsScreen
+import com.example.mindtivity.ui.theme.screens.products.ViewProductsScreen
+import com.example.mindtivity.ui.theme.screens.signup.SignupScreen
+import com.example.mindtivity.ui.theme.screens.splash.SplashScreen
+
+
+@Composable
+fun AppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination:String = ROUT_SPLASH
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+
+        composable(ROUT_ABOUT) {
+            AboutScreen(navController = navController)
+        }
+        composable(ROUT_HOME) {
+            HomeScreen(navController = navController)
+        }
+
+        composable(ROUT_SPLASH) {
+            SplashScreen(navController = navController)
+        }
+
+        composable(ROUT_SIGNUP) {
+            SignupScreen(navController = navController)
+        }
+        composable(ROUT_LOGIN) {
+           LoginScreen(navController = navController)
+        }
+        composable(ADD_PRODUCTS_URL) {
+            AddProductsScreen(navController = navController)
+        }
+        composable(VIEW_PRODUCTS_URL) {
+            ViewProductsScreen(navController = navController)
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/mindtivity/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.example.mindtivity.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+val pastorange = Color(0xFF7D5260)
+val orange = Color(0xFF7D5260)
+val ory = Color(0xFFF74A3E)
+val pastory = Color(0xFFFF7D74)
